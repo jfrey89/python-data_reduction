4c6ac24ea3eb7ec8e4ada48cd1eba0d5d4c4880f
Will Frey <jfrey89@gmail.com>
2013-04-12 03:06:39 -0400

    modified:   data_reduction/divided_difference.py

--- data_reduction/divided_difference.py
+++ data_reduction/divided_difference.py
@@ -5,8 +5,8 @@ class NewtonPoly(object):
 
     def __init__(self, x_data, y_data):
         self._x = x_data
-        self.dim = len(self._x) - 1
-        self.coeff = self._coefficients(self, x_data, y_data)
+        self.dim = len(x_data) - 1
+        self.coeff = self._coefficients x_data, y_data)
 
     def eval(self, x):
         """
