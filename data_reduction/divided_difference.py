83b28c6dbad8c105f36ce96376f355e85b154d56
Will Frey <jfrey89@gmail.com>
2013-04-12 09:49:10 -0400

    modified:   data_reduction/divided_difference.py

--- data_reduction/divided_difference.py
+++ data_reduction/divided_difference.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python -O
 
+import numpy as np
+
 
 class NewtonPoly(object):
 
     def __init__(self, x_data, y_data):
-        self._x = x_data
+        self.knots = np.column_stack((x_data, y_data))
         self.dim = len(x_data) - 1
         self.coeff = self._coefficients(x_data, y_data)
 
@@ -24,13 +26,14 @@ class NewtonPoly(object):
 
         return p
 
-    def _coefficients(self, x_data, y_data):
+    def _coefficients(self):
         """
         Computes the divided difference coefficients.
         """
 
-        m = len(x_data)     # number of data points
-        a = y_data.copy()
+        m = len(self.knots)     # number of data points
+        x_data = self.knots[:, 0]
+        a = self.knots[:, 1]
 
         for k in range(1, m):
             a[k:m] = (a[k:m] - a[k - 1]) / (x_data[k:m] - x_data[k - 1])
