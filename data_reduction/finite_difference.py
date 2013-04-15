74fa31dbdbe7ce9ebccbe015b9bb80c3c31e4122
Will Frey <jfrey89@gmail.com>
2013-04-15 05:49:28 -0400

    new file:   data_reduction/finite_difference.py

---
+++ data_reduction/finite_difference.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env -O python
+
+import numpy as np
+
+
+def finite_difference(x, y):
+    """
+    Calcualtes the 4th order finite difference.
+    """
+
+    n = len(x)
+
+    for i in np.rang
+
+    pass
