acc289f28747534964b74c81085470908fe261d9
Will Frey <jfrey89@gmail.com>
2013-04-18 15:38:08 -0400

    modified:   data_reduction/data_reduction.py

--- data_reduction/data_reduction.py
+++ data_reduction/data_reduction.py
@@ -1,14 +1,37 @@
 import numpy as np
-from scipy import interpolate
 from polynomial import NewtonPolynomial
+from scipy.interpolate import interp1d
+
+
+class ApproxDeriv(object):
+
+    def __init__(self, **args):
+
+        if 'points' in args:
+            self.points = np.array(args['points'])
+            self.xi = self.x
+        else:
+            self.points = np.array([[0, 0]])
+            self.xi = np.array([1.])
+
+    @property
+    def x(self):
+        return self.points[:, 0]
+
+    @property
+    def y(self):
+        return self.points[:, 1]
+
+    def __call__(self, x):
+        f = interp1d(self.x, self.y)
+        return f(x)
 
 
 def compute_deriv(pN, k=4):
     if not isinstance(pN, NewtonPolynomial):
         raise ValueError('Input must be Newton polynomial')
 
-    divdiff = pN.divdiffcol(k)
-
+    #divdiff = pN.divdiffcol(k+1)
 
 
 if __name__ == "__main__":
