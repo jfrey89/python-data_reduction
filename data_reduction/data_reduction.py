6c05d35aaf413a89a460b9418bc29df0435bedc6
Will Frey <jfrey89@gmail.com>
2013-04-18 13:37:28 -0400

    modified:   data_reduction/data_reduction.py 	new file:   data_reduction/newtonpolynomial.py 	modified:   data_reduction/polynomial.py

--- data_reduction/data_reduction.py
+++ data_reduction/data_reduction.py
@@ -1,3 +1,15 @@
-from polynomial import Polynomial, NewtonPolynomial
 import numpy as np
-from scipy import linalg
+from scipy import interpolate
+from polynomial import NewtonPolynomial
+
+
+def compute_deriv(pN, k=4):
+    if not isinstance(pN, NewtonPolynomial):
+        raise ValueError('Input must be Newton polynomial')
+
+    divdiff = pN.divdiffcol(k)
+
+
+
+if __name__ == "__main__":
+    pass
--- data_reduction/polynomial.py
+++ data_reduction/polynomial.py
@@ -75,6 +75,7 @@ class Polynomial(object):
         return np.linalg.eigvals(companion)
 
     def monomial_2_newton(self):
+        from newtonpolynomial import NewtonPolynomial
         return NewtonPolynomial(points=self.points)
 
     def __add__(self, other):
@@ -89,69 +90,3 @@ class Polynomial(object):
             newcoeff[len(self.coeff) - len(other.coeff):] += other.coeff
 
         return Polynomial(coeff=newcoeff)
-
-
-class NewtonPolynomial(Polynomial):
-
-    base = 'Newton'
-
-    def __init__(self, **args):
-        if 'coeff' in args:
-            try:
-                self.xi = np.array(args['xi'])
-            except KeyError:
-                raise ValueError('Coefficients need to be given together \
-                                 with abscissae values xi')
-        super(NewtonPolynomial, self).__init__(**args)
-
-    def point_2_coeff(self):
-        return np.array(list(self.divdiff()))
-
-    def divdiff(self):
-        xi = self.xi
-        row = self.y
-        yield row[0]
-
-        for level in xrange(1, len(xi)):
-            row = (row[1:] - row[:-1]) / (xi[level:] - xi[:-level])
-
-            if np.allclose(row, 0):
-                self.degree = level - 1
-                break
-
-            yield row[0]
-
-    def divdiffcol(self, depth):
-        xi = self.xi
-        row = self.y
-
-        if depth > len(xi):
-            raise ValueError('depth out of bounds')
-
-        for level in xrange(1, depth):
-            row = (row[1:] - row[:-1]) / (xi[level:] - xi[:-level])
-
-            if level == depth:
-                break
-
-            if np.allclose(row, 0):
-                raise ValueError('derivative is identically zero')
-
-        return row
-
-    def __call__(self, x):
-        # first compute the sequence 1, (x-x_1), (x-x_1)(x-x_2), ...
-        nps = np.hstack([1., np.cumprod(x - self.xi[:self.degree])])
-        return np.dot(self.coeff, nps)
-
-    def newton_2_monomial(self):
-        return Polynomial(points=self.points)
-
-    def __add__(self, other):
-        if not isinstance(other, NewtonPolynomial):
-            raise ValueError('Operands must be Newton polynomials')
-        pass
-
-    def __changepoints__(self, other):
-        pts = np.c_[self.xi, other(self.xi)]
-        return NewtonPolynomial(points=pts).coeff
