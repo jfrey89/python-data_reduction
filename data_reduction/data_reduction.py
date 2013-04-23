edf1d4f9838139fd203970e6cc5790058e3dbd78
Will Frey <jfrey89@gmail.com>
2013-04-23 12:05:11 -0400

    new file:   data_reduction/data_reduction.py

---
+++ data_reduction/data_reduction.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from norm import *
+from NewtonPolynomial import *
+
+
+if __name__ == "__main__":
+
+    #xi = np.linspace(-5, 5, 101)
+    #pts = np.c_[xi, np.sin(xi)]
+    #pN = NewtonPolynomial(points=pts)
+    #d4pN = differentiate(pN, r=4)
+    #frnm = Norm(d4pN)
+    #pass
