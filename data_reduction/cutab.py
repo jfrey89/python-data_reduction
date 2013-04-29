fb9cef2e8f4ae0a9dccb7e89563bdfae66b147f1
Will Frey <jfrey89@gmail.com>
2013-04-29 19:12:03 -0400

    modified:   data_reduction/cutab.py 	modified:   data_reduction/main.py

--- data_reduction/cutab.py
+++ data_reduction/cutab.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python
 
 import numpy as np
-import data_reduction as dr
+import data_reduction.bisection as root
 
 
 def cutab(norm, xi, eps, r):
-    C, eta = 1, 0.5
+    C, eta = 100, 0.5
     k = r - 1
     a, b = xi[0], xi[-1]
     E, T = C * eps, np.array([a])
-
-    n0 = len(xi)
     j = 1
 
     while True:
-
         if j <= n0:
             F = lambda x: np.abs(np.power(x - T[j - 1], k)) * norm(T[j - 1], x)
             if F(b) > E:
                 g = lambda x: F(x) - E
-                x_e = dr.bisection(g, T[j - 1], b)
+                x_e = root.bisection(g, T[j - 1], b)
                 T = np.append(T, x_e)
                 j += 1
-            elif F(b) == E:
+            elif np.abs(norm(T[j - 1], b) -
+                        E / np.abs(np.power(b - T[j - 1], k))) < 10e-8:
                 T = np.append(T, b)
-                n = j
                 return T
             elif F(b) < E:
                 T = np.append(T, b)
                 eps = eta * eps
                 E = C * eps
-                n = j
+                eps = eps * eta
+                T = np.array([a])
                 j = 1
-            else:
-                return False
+
         elif j > n0:
             eta = np.sqrt(eta)
             if np.abs(np.power(b - T[n0 - 1], 3)) * norm(T[n0 - 1], b) < E:
@@ -41,4 +38,7 @@ def cutab(norm, xi, eps, r):
             else:
                 eps = eps / eta
 
+            T = np.array([a])
             j = 1
+
+    return T
--- data_reduction/main.py
+++ data_reduction/main.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 
 import numpy as np
-import data_reduction as dr
+import data_reduction.polynomial as poly
+import data_reduction.dr_dxr as deriv
+import data_reduction.norm as norm
 
 # main()
 if __name__ == "__main__":
+    import data_reduction.cutab as cut
     # dummy data
-    tau = 10e-8
+    tau = 10e-4
     eps = tau
     xi = np.linspace(-5, 5, 101)
     yi = 1 / (1 + np.power(xi, 2))
     knots = np.c_[xi, yi]
     r = 4
-    p = dr.Newton(points=knots)
-    d4p = dr.dr_dxr(p, r=4)
-    norm = dr.Norm(d4p)
-    #g = lambda x: np.abs(np.power(x - xi[0], 3)) * norm(xi[0], x)
-    #h = lambda x: g(x) - 0.1
-    #x_e = bisection(h, xi[0], xi[-1])
-
-    # First run of middle loop
-
+    p = poly.Newton(points=knots)
+    d4p = deriv.dr_dxr(p, r=4)
+    norm = norm.Norm(d4p)
+    T = cut.cutab(norm, xi, eps, r)
     pass
