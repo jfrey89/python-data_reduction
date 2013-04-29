273c37cdc16d16441f989bd8debab5455a39a85a
Will Frey <jfrey89@gmail.com>
2013-04-29 12:01:23 -0400

    modified:   data_reduction/cutab.py 	renamed:    data_reduction/data_reduction.py -> data_reduction/main.py

--- data_reduction/cutab.py
+++ data_reduction/cutab.py
@@ -4,50 +4,40 @@ import numpy as np
 from data_reduction.bisection import bisection
 
 
-def F(norm, a, b, k):
-    return np.power(np.abs(b - a), k) * norm(a, b)
-
-
 def cutab(norm, xi, eps, r, n0):
-    C = 100
     k = r - 1
     b = xi[-1]
-    T = np.array([xi[0]])
-    j = 1
-    eta = 0.5
+    C = 100
 
-    while True:
-        F = lambda x: np.abs(np.power(x - T[j-1], k)) * norm(T[j-1], x)
-        E = C * eps
-        
-        if j <= n0:
-            if F(b) > E:
-                print "Okay, doing my thing."
-                g = lambda x: g(x) - E
-                x_e = bisection(g, T[j-1], b)
-                T = np.append(T, x_e)
-                j += 1
-            elif F(b) == E:
-                print "Yea, we got it!"
-                T = np.append(T, b)
-                return T
-            elif F(b) < E:
-                print "This part sucks."
-                T = np.append(T, b)
-                if j <= n0:
-                    print "This is Path 1."
-                    eps = eta * eps
-                    T = np.array([xi[0]])
-                    print "We reset the loop."
-                    j = 1
-            else:
-                print "You derp'd. Good job."
-                return False
-        elif j > n0:
-            print "Okay, Path 2."
+    E = C * eps
+    n0 = len(xi)
+
+
+
+
+def cutab_inner(F, E, n0, a, b, eps):
+    T = np.array([a])
+    j = 1
+
+    while j <= n0:
+        # Define our interval weighting function
+        F = lambda x: np.abs(np.power(x - T[j - 1], k)) * norm(T[j - 1], x)
+
+        if F(b) > E:
+            # define function to find roots of
+            # note that it is monotone increasing
+            g = lambda x: F(x) - E
+            x_e = bisection(g, T[j - 1], b)
+            T = np.append(T, x_e)
+            j += 1
+        elif F(b) == E:
+            T = np.append(T, b)
+            n = j
+            return T, n
+        elif F(b) < E:
+            T = np.append(T, b)
+            n = j
+            eps = 0.5 * eps
         else:
-            print "You fucked it up."
-            
-        
-            
-    
+            print "It broke."
+            return False
--- data_reduction/main.py
+++ data_reduction/main.py
@@ -5,19 +5,26 @@ from data_reduction.polynomial import Newton
 from data_reduction.dr_dxr import dr_dxr
 from data_reduction.norm import Norm
 from data_reduction.bisection import bisection
+from data_reduction.cutab import cutab
 
 # main()
 if __name__ == "__main__":
     # dummy data
+    tau = 10e-4
+    eps = tau
     xi = np.linspace(-5, 5, 101)
     yi = 1 / (1 + np.power(xi, 2))
     knots = np.c_[xi, yi]
     r = 4
-
     p = Newton(points=knots)
     d4p = dr_dxr(p, r=4)
     norm = Norm(d4p)
-    g = lambda x: np.abs(np.power(x - xi[0], 3)) * norm(xi[0], x)
-    h = lambda x: g(x) - 0.1
-    x_e = bisection(h, xi[0], xi[-1])
+    #g = lambda x: np.abs(np.power(x - xi[0], 3)) * norm(xi[0], x)
+    #h = lambda x: g(x) - 0.1
+    #x_e = bisection(h, xi[0], xi[-1])
+
+    # First run of middle loop
+    n0 = len(xi)
+    cutab(norm, xi, eps, r, n0)
+
     pass
