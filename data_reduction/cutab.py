42932ac3520b6232d0f5eeda00d454bed9af81dc
Will Frey <jfrey89@gmail.com>
2013-04-30 12:16:59 -0400

    modified:   .gitignore 	modified:   data_reduction/cutab.py 	modified:   data_reduction/main.py

--- data_reduction/cutab.py
+++ data_reduction/cutab.py
@@ -5,27 +5,21 @@ import data_reduction.bisection as root
 
 
 def cutab(norm, xi, eps, r):
-    C = 5000
+    C = 10000
     eta = 0.5
     k = r - 1
     T = np.zeros(np.shape(xi))
     a, b = xi[0], xi[-1]
-    print '*' * 20
-    print "Initialized"
-    print '*' * 20
-    print "\n\n"
 
     # Statement (1)
     E = C * eps
 
-    print "First run."
     T[0] = a
     j = 1
     F = lambda x: np.abs(np.power(x - T[j - 1], k)) * norm(T[j - 1], x)
 
     while True:
         if F(b) > E:
-            print "Adding a new point."
             g = lambda x: F(x) - E
             x_e = root.bisection(g, T[j - 1], b)
             T[j] = x_e
@@ -33,22 +27,19 @@ def cutab(norm, xi, eps, r):
         elif np.abs(
             norm(T[j - 1], b) - E / np.abs(np.power(b - T[j - 1], k))
         ) < 10e-8:
-            print "Equality reached.\nTerminating!"
             T[j] = b
             n = j
             return np.trim_zeros(T, 'b')
         elif F(b) < E:
-            print "Adjusting epsilon."
+            #print "shrink eps: %e -> %e" % (eps, eps * eta)
             T[j] = b
             n = j
             eps = eps * eta
             break
-        else:
-            print "It broke."
 
     n0 = n
 
-    for iteration in range(0, 10):
+    for iteration in range(0, 100):
         E = C * eps
         T = np.zeros(np.shape(T))
         T[0] = a
@@ -75,11 +66,12 @@ def cutab(norm, xi, eps, r):
                 eta = np.sqrt(eta)
                 F = np.abs(np.power(b - T[n0 - 1], k)) * norm(T[n0 - 1], b)
                 if F < E:
-                    print "too large"
+                    #print "shrink eps: %e -> %e" % (eps, eps * eta)
                     eps = eps * eta
                     break
                 else:
-                    print "too small"
+                    #print "grow eps: %e -> %e" % (eps, eps / eta)
                     eps = eps / eta
                     break
+
     return np.trim_zeros(T, 'b')
--- data_reduction/main.py
+++ data_reduction/main.py
@@ -4,28 +4,49 @@ import numpy as np
 import data_reduction.polynomial as poly
 import data_reduction.dr_dxr as deriv
 import data_reduction.norm as norm
+import scipy.interpolate.fitpack as fitpack
 import matplotlib.pyplot as plt
 
 # main()
 if __name__ == "__main__":
     import data_reduction.cutab as cut
     # dummy data
-    tau = 10e-4
-    eps = tau
+    run_count = 1
+    r = 4
+    tol = 10e-4
+    eps = tol
     xi = np.linspace(-5, 5, 101)
     yi = 1 / (1 + np.power(xi, 2))
-    #yi = np.sin(xi)
-    knots = np.c_[xi, yi]
-    r = 4
-    p = poly.Newton(points=knots)
+    data = np.c_[xi, yi]
+
+    p = poly.Newton(points=data)
     d4p = deriv.dr_dxr(p, r=4)
     norm = norm.Norm(d4p)
+    print "Run %d" % run_count
     T = cut.cutab(norm, xi, eps, r)
-    f = lambda x: 1 / (1 + np.power(x, 2))
+
+    tck = fitpack.splrep(xi, yi, t=T[1:-1])
+    fit = fitpack.splev(xi, tck)
+    error = np.abs(fit - yi).max()
+    print "Error: %e\n" % error
+
+    while error > tol:
+        run_count += 1
+        print "Run %d" % run_count
+        eps = eps / 2
+        T = cut.cutab(norm, xi, eps, r)
+        tck = fitpack.splrep(xi, yi, t=T[1:-1])
+        fit = fitpack.splev(xi, tck)
+        error = np.abs(fit - yi).max()
+        print "Error %e\n" % error.max()
+
+    plt.figure()
+    plt.plot(xi, yi, 'r')
+    f = lambda x: 1 / (1 + x ** 2)
     Ty = np.zeros(len(T))
     for i in range(0, len(T)):
-        Ty[i] = f(T[i])
-    plt.figure()
-    plt.plot(p.x, p.y)
+            Ty[i] = f(T[i])
+
     plt.plot(T, Ty, 'ro')
+    plt.plot(xi, fit, 'g^--')
     pass
